99ca315787ba63a0035d89b1ffeb7b2124bb3bf5
zhaolongkzz <437866724@qq.com>
2019-07-05 21:01:47 +0800

    fix

--- bvh2data.py
+++ bvh2data.py
@@ -1,5 +1,6 @@
 import math
 import os
+import string
 import argparse
 # import tf
 import numpy as np
@@ -42,17 +43,31 @@ class BVHReader:
         self.tokenlist = []
         self.linenr = 0
 
+        self._root = None
+        self._nodestack = []
+        self._numchannels = 0
+
+    def onHierarchy(self, root):
+        pass
+
+    def onMotion(self, frames, dt):
+        pass
+
+    def onFrame(self, values):
+        pass
+
     def read(self):
-        self.fhandle = file(self.filename)
+        # self.fhandle = file(self.filename)
+        self.fhandle = open(self.filename, 'r')
 
         self.readHierarchy()
         self.onHierarchy(self._root)
         self.readMotion()
-
+    
     def readHierarchy(self):
         # FIRST read 'HIERARCHY' as the begining
         tok = self.token()
-        if tok != 'HERARCHY':
+        if tok != 'HIERARCHY':
             raise SyntaxError("Syntax error in line %d: 'HIERARCHY' expected, got '%s' instead"%(self.linenr, tok))
         
         # then put inside and read 'ROOT'
@@ -67,8 +82,6 @@ class BVHReader:
         self.readNode()
     
     def readMotion(self):
-        """Read the motion samples.
-        """
         # No more tokens (i.e. end of file)? Then just return 
         try:
             tok = self.token()
@@ -103,26 +116,13 @@ class BVHReader:
             a = s.split()
             if len(a)!=self._numchannels:
                 raise SyntaxError("Syntax error in line %d: %d float values expected, got %d instead"%(self.linenr, self._numchannels, len(a)))
-            # maps a to float(x)
-            values = map(lambda x: float(x), a)
+            values = list(map(lambda x: float(x), a))
+            # map function => Python 2 return lists; Python 3 return iterators, so need a list
             self.onFrame(values)
-
-    def onHierarchy(self, root):
-        self.scaling_factor = 0.1 / root.children[0].children[0].offset[0]
-    
-    def onMotion(self, frame, dt):
-        self.dt = dt
-        self.num_motions = frames
-
-    def onFrame(self, values):
-        self.all_motions.append(values)
-
-    def readMotion(self):
-        pass
 
     def readNode(self):
         name = self.token()
-        self._nodestack[-1],name = name
+        self._nodestack[-1].name = name
 
         tok = self.token()
         if tok != "{":
@@ -165,7 +165,7 @@ class BVHReader:
                 raise SyntaxError("Syntax error in line %d: Unknown keyword '%s'"%(self.linenr, tok))
 
     def token(self):
-        if elf.tokenlist != []:
+        if self.tokenlist != []:
             tok = self.tokenlist[0]
             self.tokenlist = self.tokenlist[1:]
             return tok
@@ -181,7 +181,7 @@ class BVHReader:
         # read next line
         while 1:
             s = self.fhandle.readline()
-            self.linear += 1
+            self.linenr += 1
             if s == '':
                 raise StopIteration
             return s
@@ -209,11 +209,14 @@ class BVHReader:
             raise SyntaxError("Syntax error in line %d: Float expected, got '%s' instead"%(self.linenr, tok))
 
 
-class BVHBroadcaster(BVHReader):
-    def __int__(self, filename, root_frame):
+class BVHTransfer(BVHReader):
+    def __init__(self, filename, root_frame):
         BVHReader.__init__(self, filename)
         # self.br = tf.TransformBroadcaster()
+        # all the motions of BVH files
         self.all_motions = []
+        # what the joint part I need for mimicking
+        self.need_motions = []
         self.dt = 0.5
         self.num_motions = 1
         self.root_frame = root_frame
@@ -222,63 +225,178 @@ class BVHBroadcaster(BVHReader):
         self.this_motion = None
 
         self.scaling_factor = 0.1
-    
-    def onHierarchy(self, root):
-        self.scaling_factor = 0.1/root.children[0].children[0].offset[0]
 
+    def onHierarchy(self, root):
+        self.scaling_factor = 0.1 / root.children[0].children[0].offset[0]
+    
     def onMotion(self, frames, dt):
         self.dt = dt
         self.num_motions = frames
 
-    def read_file(self, filename):
-        with open(filename, 'r') as fr:
-            fo = fr.readline()
-
-    def broadcast(self, loop=True):
-        self.read()
-        rate = rospy.rate(1 / self.dt)
-
-        while not rospy.is_shutdown():
-            for ind in range(self.num_motions):
-                self.counter = 0
-                self.this_motion = self.all_motion[ind]
-
-                self.broadcastRootJoint(self._root, self.root_frame)
-                if rospy.is_shutdown():
-                    break
-                rate.sleep()
-            if not loop:
-                break
+    def onFrame(self, values):
+        self.all_motions.append(values)
             
-    def broadcastRootJoint(self, root, root_frame):
+    def rootJoint(self, root, root_frame):
         if root.isEndSite():
             return
         
         num_channels = len(root.channels)
+        temp = []
+        flag_trans = 0
+        flag_rot = 0
 
+        need_joints = [ "Hips", "Spine1", "Neck1", 
+                        "RightUpLeg", "RightLeg", "RightFoot", "RightArm", "RightForeArm",
+                        "LeftUpLeg", "LeftLeg", "LeftFoot", "LeftArm", "LeftForeArm"]
 
-def rotmat2quat(R):
-    """
-    converts a rotation matrix to a quaternion
-    """
-    rotdiff = R - R.T
+        mat_rot = np.array([ [1.,0.,0.,0.], 
+                             [0.,1.,0.,0.], 
+                             [0.,0.,1.,0.], 
+                             [0.,0.,0.,1.] ])
 
-    r = np.zeros(3)
-    r[0] = -rotdiff[1, 2]
-    r[1] =  rotdiff[0, 2]
-    r[2] = -rotdiff[0, 1]
+        for channel in root.channels:
+            keyval = self.this_motion[self.counter]
+            if(channel == "Xposition"):
+                flag_trans = True
+                x = keyval
+                self.need_motions.append(self.dt)
+                self.need_motions.append(x)
+            elif(channel == "Yposition"):
+                flag_trans = True
+                y = keyval
+                self.need_motions.append(y)
+            elif(channel == "Zposition"):
+                flag_trans = True
+                z = keyval
+                self.need_motions.append(z)
+            elif(channel == "Xrotation"):
+                flag_rot = True
+                xrot = keyval
+                temp.append(xrot)
+                theta = math.radians(xrot)
+                c = math.cos(theta)
+                s = math.sin(theta)
+                mat_x_rot = np.array([ [1.,0.,0.,0.], 
+                                       [0.,c,-s, 0.], 
+                                       [0.,s, c, 0.], 
+                                       [0.,0.,0.,1.] ])
+                mat_rot = np.matmul(mat_rot, mat_x_rot)
 
-    sin_theta = np.linalg.norm(r) / 2
-    r0 = np.divide(r, np.linalg.norm(r) + np.finfo(np.float32).eps)
-    cos_theta = (np.trace(R) - 1) / 2
+            elif(channel == "Yrotation"):
+                flag_rot = True
+                yrot = keyval
+                temp.append(yrot)
+                theta = math.radians(yrot)
+                c = math.cos(theta)
+                s = math.sin(theta)
+                mat_y_rot = np.array([ [c, 0.,s, 0.],
+                                       [0.,1.,0.,0.],
+                                       [-s,0.,c, 0.],
+                                       [0.,0.,0.,1.] ])
+                mat_rot = np.matmul(mat_rot, mat_y_rot)
 
-    theta = np.arctan2( sin_theta, cos_theta )
+            elif(channel == "Zrotation"):
+                flag_rot = True
+                zrot = keyval
+                temp.append(zrot)
+                theta = math.radians(zrot)
+                c = math.cos(theta)
+                s = math.sin(theta)
+                mat_z_rot = np.array([ [c,-s, 0.,0.],
+                                       [s, c, 0.,0.],
+                                       [0.,0.,1.,0.],
+                                       [0.,0.,0.,1.] ])
+                mat_rot = np.matmul(mat_rot, mat_z_rot)
+            else:
+                return
+            self.counter += 1
+        
+        if flag_trans:
+            temp_trans = (self.scaling_factor * (x + root.offset[0]), 
+                          self.scaling_factor * (y + root.offset[1]), 
+                          self.scaling_factor * (z + root.offset[2]))
+        else:
+            temp_trans = (self.scaling_factor * (root.offset[0]), 
+                          self.scaling_factor * (root.offset[1]), 
+                          self.scaling_factor * (root.offset[2]))
+                    
+        temp_rot = self.quaternion_from_matrix(mat_rot)
+        if root_frame in need_joints:
+            self.need_motions.extend(temp_rot)
+
+        # temp_rot = tf.transformations.quaternion_from_matrix(mat_rot)
+        # self.br.sendTransform(temp_trans, temp_rot, rospy.Time.now(), root.name, parent_frame)
+
+        for each_child in root.children:
+            self.rootJoint(each_child, root.name)
+
+    def quaternion_from_matrix(self, matrix):
+        """Return quaternion from rotation matrix.
+
+        >>> R = rotation_matrix(0.123, (1, 2, 3))
+        >>> q = quaternion_from_matrix(R)
+        >>> numpy.allclose(q, [0.0164262, 0.0328524, 0.0492786, 0.9981095])
+        True
+
+        """
+        q = np.empty((4, ), dtype=np.float64)
+        M = np.array(matrix, dtype=np.float64, copy=False)[:4, :4]
+        t = np.trace(M)
+        if t > M[3, 3]:
+            q[3] = t
+            q[2] = M[1, 0] - M[0, 1]
+            q[1] = M[0, 2] - M[2, 0]
+            q[0] = M[2, 1] - M[1, 2]
+        else:
+            i, j, k = 0, 1, 2
+            if M[1, 1] > M[0, 0]:
+                i, j, k = 1, 2, 0
+            if M[2, 2] > M[i, i]:
+                i, j, k = 2, 0, 1
+            t = M[i, i] - (M[j, j] + M[k, k]) + M[3, 3]
+            q[i] = t
+            q[j] = M[i, j] + M[j, i]
+            q[k] = M[k, i] + M[i, k]
+            q[3] = M[k, j] - M[j, k]
+        q *= 0.5 / math.sqrt(t * M[3, 3])
+        return q
     
-    # compute the quaternion
-    q     = np.zeros(4)
-    q[0]  = np.cos(theta/2)
-    q[1:] = r0 * np.sin(theta/2)
-    return q
+    def order_motions(self):
+        pass
+    
+    def transfer(self, output_name):
+        self.read()
+        for ind in range(self.num_motions):
+            self.counter = 0
+            self.this_motion = self.all_motions[ind]
+
+            self.rootJoint(self._root, self.root_frame)
+            
+            # transfer data to quaternion
+            # quat = rotmat2quat(R)
+
+    def rotmat2quat(self, R):
+        """
+        converts a rotation matrix to a quaternion
+        """
+        rotdiff = R - R.T
+
+        r = np.zeros(3)
+        r[0] = -rotdiff[1, 2]
+        r[1] =  rotdiff[0, 2]
+        r[2] = -rotdiff[0, 1]
+
+        sin_theta = np.linalg.norm(r) / 2
+        r0 = np.divide(r, np.linalg.norm(r) + np.finfo(np.float32).eps)
+        cos_theta = (np.trace(R) - 1) / 2
+
+        theta = np.arctan2( sin_theta, cos_theta )
+        
+        # compute the quaternion
+        q     = np.zeros(4)
+        q[0]  = np.cos(theta/2)
+        q[1:] = r0 * np.sin(theta/2)
+        return q
 
 def save_file(name, lists=[[1,2,3],[2,3,4]]):
     """
@@ -292,35 +410,33 @@ def save_file(name, lists=[[1,2,3],[2,3,4]]):
         file_handle.write(str(lists[i]) + ',\n')
     file_handle.write(']\n}')
     file_handle.close()
-    print('Save motion file to format of Mimic~')
+    print('Save motion file to format of Mimic~\n')
 
 def argsparser():
     parser = argparse.ArgumentParser("python BVHBroadcaster.py bvh_file base_frame --name --loop")
     parser.add_argument('bvh_file', default="./data/mocaps/22_13.bvh", help="A path to bvh file that you want to broadcast")
-    parser.add_argument('file_name', default="backfilp", help="the name of bvh file")
+    # parser.add_argument('file_name', default="backfilp", help="the name of bvh file")
     parser.add_argument('base_frame', default="world", help="An existing frame in rviz on which the skeleton will be loaded")
-    parser.add_argument('-n', '--name', default="BVHBroadcaster", help="Node name, default: BVHBroadcaster")
-    parser.add_argument('-l', '--loop', action="store_true", help="Loop broadcasting")
+    parser.add_argument('-n', '--name', default="backfilp", help="Node name, default: BVHBroadcaster")
+    # parser.add_argument('-l', '--loop', action="store_true", help="Loop broadcasting")
     return parser.parse_args()
 
 def main(args):
-    print(args.name)
-    # bvh_test = BVHBroadcaster(args.bvh_file, args.base_frame)
-    print("Broadcasting bvh file {1} ({0}) on frame {2}".format(args.bvh_file, args.file_name, args.base_frame))
-    if args.loop:
-        print("Loop")
-    else:
-        print("Only once")
-    # bvh_test.broadcast(loop = args.loop)
-
-
-
-    # TODO: make data to quaternion
-    # rotmat2quat(R)
+    print("Name of output: cmu_{}".format(args.name))
+    bvh2mimic = BVHTransfer(args.bvh_file, args.base_frame)
+    print("Broadcasting bvh file: cmu_{1} ({0}) on frame {2}".format(args.bvh_file, args.name, args.base_frame))
+    
+    bvh2mimic.transfer(args.name)
+    
+    # if args.loop:
+    #     print("Loop")
+    # else:
+    #     print("Only once")
+    # bvh2mimic.broadcast(loop = args.loop)
 
     # TODO: lists
     save_file(args.file_name, lists)
-    print("Broadcasting done and exit~")
+    print("Transfering done and exit~\n")
 
 def test():
     pass
@@ -329,25 +445,3 @@ if __name__ == '__main__':
     args = argsparser()
     # test(args)
     main(args)
-
-
-'''
-TODO: Data transfer
-[
-	duration of frame in seconds (1D),
-	root position (3D),
-	root rotation (4D),
-	chest rotation (4D),
-	neck rotation (4D),
-	right hip rotation (4D),
-	right knee rotation (1D),
-	right ankle rotation (4D),
-	right shoulder rotation (4D),
-	right elbow rotation (1D),
-	left hip rotation (4D),
-	left knee rotation (1D),
-	left ankle rotation (4D),
-	left shoulder rotation (4D),
-	left elbow rotation (1D)
-]
-'''
